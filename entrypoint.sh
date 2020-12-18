b2265d14ebae3dec7b8ba3f7ff5735b39522821b
Mohammed Ajil <mohammed@ajil.ch>
2020-12-18 12:03:05 +0100

    increase timeout for large downloads

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 crontab /var/spool/cron/crontabs/root
 /etc/init.d/cron start
-gunicorn --log-level INFO --timeout 6000 --workers 4 --bind 0.0.0.0:5000 "filedrop.app:create_app()"
+gunicorn --log-level INFO --timeout 100000 --workers 4 --bind 0.0.0.0:5000 "filedrop.app:create_app()"
